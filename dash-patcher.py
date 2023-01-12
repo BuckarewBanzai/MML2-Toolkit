dad2f6f6514046bae1d7d3cd8236a5f94fbfa9f7
josh <itzdi0de@gmail.com>
2023-01-12 12:25:49 -0500

    Update dash-patcher.py

--- dash-patcher.py
+++ dash-patcher.py
@@ -1,3 +1,12 @@
+import sys
+
+def printHelp():
+    print("Usage: dash-patcher.py [OPTIONS]\n")
+    print("Options: ")
+    print("patch        Patch dash2.dll into dash2.exe")
+    print("unpatch      Restore dash2.exe back to default")
+    print("-h           Print help menu")
+
 #mml2 exe we want to patch
 pe = 'dash2.exe'
 
@@ -19,30 +28,67 @@ def patchCheck(dll):
     elif dll == "ole32.dll":
         return False
 
-def patch():
+def patchDll(f):
     f.seek(0x23083E, 0)
     f.write(str.encode("dash2.dll"))
 
-def unpatch():
+def unpatchDll(f):
     f.seek(0x23083E, 0)
     f.write(str.encode("ole32.dll"))
 
-with open(pe, "r+b") as f:
-    f.seek(0x23083E, 0)
-    dll = f.read(9)
-    print(byte2ascii(dll))
-
-    if patchCheck(dll):
-        print("Game is already patched.")
-    
-    else:
-        patch()
+def patch():
+    with open(pe, "r+b") as f:
         f.seek(0x23083E, 0)
         dll = f.read(9)
 
         if patchCheck(dll):
-            print("Game patched successfully!")
+            print("Game is already patched.")
+            return
+    
+        else:
+            patchDll(f)
+            f.seek(0x23083E, 0)
+            dll = f.read(9)
+
+            if patchCheck(dll):
+                print("Game patched successfully!")
+
+            if not patchCheck(dll):
+                print("Patching failed. Please restore dash2.exe from a backup and try again.")
+
+
+def unpatch():
+    with open(pe, "r+b") as f:
+        f.seek(0x23083E, 0)
+        dll = f.read(9)
 
         if not patchCheck(dll):
-            print("Patching failed. Please restore dash2.exe from a backup and try again.")
-            print(byte2ascii(dll))
+            print("No patch detetcted.")
+            return
+    
+        else:
+            unpatchDll(f)
+            f.seek(0x23083E, 0)
+            dll = f.read(9)
+
+            if patchCheck(dll):
+                print("Patching revert failed. Please restore dash2.exe from a backup")
+
+            if not patchCheck(dll):
+                print("Patch reverted successfully!")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        patchMode = sys.argv[1]
+    else:
+        printHelp()
+        exit()
+
+    if patchMode == "patch":
+        patch()
+    elif patchMode == "unpatch":
+        unpatch()
+    else:
+        printHelp()
