8bdee611ad74fe938e29d53c67b9aa82a472f1a9
josh <itzdi0de@gmail.com>
2023-01-13 11:42:41 -0500

    Clean up unused code
    clean up unused code and add some output

--- dash2/dash2/dllmain.cpp
+++ dash2/dash2/dllmain.cpp
@@ -9,14 +9,7 @@
 #pragma comment(linker,"/export:CoInitialize=ole32.CoInitialize,@927")
 
 
-// Dummy function for entry point
-__declspec(dllexport) VOID SulferBottom() {
-
-}
-
-
-//hmodule = dll base address
-BOOL InitInstance(HINSTANCE hModule)
+DWORD WINAPI dashMain(HINSTANCE hModule)
 {
     AllocConsole();
 
@@ -25,7 +18,14 @@ BOOL InitInstance(HINSTANCE hModule)
     freopen_s(&fdum, "conout$", "w", stdout);
     freopen_s(&fdum, "conout$", "w", stderr);
 
-    std::cout << "HyperShell v0.1";
+    uintptr_t moduleBase = (uintptr_t)GetModuleHandle(L"dash2.dll");
+
+    std::cout << "HyperShell v0.1\n\n";
+    std::cout << "DLL Base: ";
+    std::cout << moduleBase;
+    std::cout << "\n";
+    std::cout << "Process Base: ";
+    std::cout << hModule;
 
     return true;
 }
@@ -38,7 +38,8 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        return InitInstance(hModule);
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)dashMain, hModule, 0, nullptr));
+        //return InitInstance(hModule);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
